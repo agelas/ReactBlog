983f6a34edcadeae605ef94d10159f914561d227
Mathias <mathiaswi177@gmail.com>
2020-06-18 16:26:12 -0700

    Added posts component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,35 @@
 import React, {Component} from 'react';
 import Header from "./components/Header"
+import Posts from "./components/Posts"
 import './App.css';
 
 class App extends Component {
+  state = {
+    posts: [
+      {
+        id: 1,
+        title: "Hello World",
+        content:"Oof."
+      },
+      {
+        id: 2,
+        title: "Hello There",
+        content: "ha"
+      },
+      {
+        id: 3,
+        title: "Dear Diary",
+        content: "I am suffering."
+      }
+    ]
+  }
+
   render() {
     return(
       <div className="App">
       APP HERE
       <Header />
+      <Posts posts={this.state.posts} />
       </div>
     );
   }
